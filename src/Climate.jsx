a002f7d87bc6dbdd83bca89088f6b0c00f91c1c9
mr-config-pe <mr.configperu@gmail.com>
2022-10-30 00:40:46 -0500

    App Temperature Modo Payaso

--- src/Climate.jsx
+++ src/Climate.jsx
@@ -7,45 +7,67 @@ const Climate = () => {
     //Estado para modificar API
 
     const [weather, setWeather] = useState({});
+
+    //Estado para modificar Temperatura
+
     const [isKelvin, setIsKelvin] = useState(true);
-    
-    
+
     //Estado para Ejecutar una sola ves
-    
-    useEffect(()=>{
-        
+
+    useEffect(() => {
+
         const success = pos => {
-            
+
             const lat = pos.coords.latitude
             const lon = pos.coords.longitude
             axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=427e8df36fbc647de7efab15c8519c8b`)
-            .then(res => setWeather(res.data))
+                .then(res => setWeather(res.data))
         }
-        
+
         navigator.geolocation.getCurrentPosition(success);
-        
+
     }, [])
-    
-    //Cambio de Kelvin a Celsius
- 
+
+    //Flag 
+
+    const flagIcon = `https://countryflagsapi.com/svg/${weather.sys?.country}`
+
+    //Climate Icon
+
+    const iconClimate = `http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`;
+
+    //Fahrenheit to Celsius
+
+    const celsius = Math.ceil(weather.main?.temp - 273.15)
+    const fahrenheit = Math.ceil(((weather.main?.temp - 273.15) * 1.8) + 32)
+
+    //Funcion para el Boton
 
     const changeTemp = () => {
         setIsKelvin(!isKelvin)
     }
 
-    console.log(weather);
+    // console.log(weather);
 
     return (
         <div>
-            <h1>App Temperature</h1>
-            {weather.name} {" "} {weather.sys?.country}
-            <h3>Clima PNG</h3>
-            <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}></img>
-            <h3>Flag Country</h3>
-            <img src={`https://countryflagsapi.com/png/${weather.sys?.country}`}></img>
-            <h3>Temperature Kelvin/Celcius</h3>
-            <a>{isKelvin ? "Celsius" : "Kelvin"}</a>{isKelvin ? weather.main?.temp -273.15 : weather.main?.temp}
-            <button onClick={changeTemp}>{isKelvin ? "Cambiar a Kelvin" : "Cambiar a Celsius"}</button>
+            <div className="container-climate">
+                <h2>App Temperature</h2>
+                <div className="country-city">
+                    <p className="p-cc1"><a>Country : </a>{weather.sys?.country}<img className="flag" src={flagIcon} /></p>
+                    <p className="p-cc2"><a>City : </a>{weather.name}</p>
+                </div>
+                <div className="celsius-fahrenheit">
+                    <a className="num-temp">{isKelvin ? celsius : fahrenheit}</a>
+                    <p className="symbol-temp">{isKelvin ? "°C" : "°F"}</p>
+                </div>
+                <div className="icon-climate">
+                    <img className="icon-climate-1" src={iconClimate} />
+                </div>
+                <div className="button-one">
+                    <button className="btn-changetemp" onClick={changeTemp}>{isKelvin ? "Change to Fahrenheit" : "Change to Celsius"}</button>
+                </div>
+            </div>
         </div>
     );
 };
